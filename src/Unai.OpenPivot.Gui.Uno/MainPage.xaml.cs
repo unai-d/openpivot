831772035e8b8cc47055201cfd5dbdf5e34a1dd8
Unai Domínguez <unai55@outlook.es>
2025-04-03 17:29:31 +0200

    Fix circle borders not using their figure segment color.

--- src/Unai.OpenPivot.Gui.Uno/MainPage.xaml.cs
+++ src/Unai.OpenPivot.Gui.Uno/MainPage.xaml.cs
@@ -1,24 +1,14 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Display;
-using SkiaSharp.Views;
 using SkiaSharp;
 using SkiaSharp.Views.Windows;
 using Windows.Storage.Pickers;
 using System.Threading.Tasks;
-using System.Numerics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -101,17 +91,6 @@ public sealed partial class MainPage : Page
 
 		if (_pivFile == null)
 		{
-			var paint = new SKPaint
-			{
-				Color = SKColors.Black,
-				IsAntialias = true,
-				Style = SKPaintStyle.Fill,
-				TextAlign = SKTextAlign.Center,
-				TextSize = 24
-			};
-			var coord = new SKPoint(scaledSize.Width / 2, (scaledSize.Height + paint.TextSize) / 2);
-			canvas.DrawText("Load a Pivot project file", coord, paint);
-
 			return;
 		}
 
@@ -204,7 +183,7 @@ public sealed partial class MainPage : Page
 
 					// Draw Stroke
 					skPaint.Style = SKPaintStyle.Stroke;
-					skPaint.Color = SKColors.Black;
+					skPaint.Color = Utils.Vector4ToSKColor(segment.Color);
 					canvas.DrawCircle(skMidPoint, circleRadius, skPaint);
 					break;
 			}
